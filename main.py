7dd849ec383b821e27d4588c04c3ff72415856ba
edayot <pro.e.dayot@gmail.com>
2024-12-15 11:42:21 +0100

    chore: update model_resolver

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 import shlex
-from model_resolver.cli import main as model_resolver_main
+from pathlib import Path
+from beet import ProjectConfig, run_beet
+
 
 def main(release: str, devmode: bool = False):
     print(f"Release {release}!")
@@ -14,40 +16,50 @@ def main(release: str, devmode: bool = False):
     os.system(f"git clone -b {cloned_branch} {git_url}")
     os.chdir("renders")
     # check if the release tag already exists
-    if os.system(f"git rev-parse --verify --quiet {release}-renders") == 0 and not devmode:
+    if (
+        os.system(f"git rev-parse --verify --quiet {release}-renders") == 0
+        and not devmode
+    ):
         print(f"Release {release} already exists!")
         return
 
-    cwd = os.getcwd()
+    cwd = Path(os.getcwd())
+    load_dir = cwd / "resourcepack"
+    print(f"Running beet in {cwd}")
     os.system("rm -rf resourcepack")
     os.makedirs("resourcepack", exist_ok=True)
-    model_resolver_main(
-        load_vanilla=True,
-        minecraft_version=release,
-        output_dir=cwd,
-        load_dir=f"{cwd}/resourcepack",
-        disable_missing_texture_error=True,
+    config = ProjectConfig(
+        pipeline=[
+            "model_resolver.plugins.render_all_vanilla",
+            "model_resolver.plugins.render_all_items",
+        ],
+        output=cwd,
+        meta={"model_resolver": {"minecraft_version": release}},
+        resource_pack={"load": load_dir, "name": load_dir.name},
     )
-    
+    with run_beet(config=config) as ctx:
+        pass
+
     if not devmode:
         os.system("git add .")
         os.system(f"git commit -m '✨ Generate renders for {release}' --allow-empty")
-        os.system(f"git tag -a '{release}-renders' -m '✨ Generate renders for {release}'")
+        os.system(
+            f"git tag -a '{release}-renders' -m '✨ Generate renders for {release}'"
+        )
 
         if "GITHUB_TOKEN" in os.environ:
             token = os.environ["GITHUB_TOKEN"]
-            os.system(f"git remote set-url origin https://github-actions:{token}@{git_url.replace('https://', '')}")
+            os.system(
+                f"git remote set-url origin https://github-actions:{token}@{git_url.replace('https://', '')}"
+            )
         os.system("git push origin renders --tags")
         os.chdir("../..")
         os.system("rm poetry.lock")
         os.system("rm -rf branch")
 
 
-
-
-
 if __name__ == "__main__":
-    release = os.getenv("MC_VERSION", "24w45a")
+    release = os.getenv("MC_VERSION", "1.21.4")
     if release is None or len(release) == 0:
         raise ValueError(f"MC_VERSION is not set, got {release}")
     if not "," in release:
