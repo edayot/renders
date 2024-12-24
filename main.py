56e893e4362ccee022fe769c2e5c740ddb264e45
edayot <pro.e.dayot@gmail.com>
2024-12-24 19:29:02 +0100

    fix: using good generator

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from contextlib import contextmanager
 
 devmode = False
 
+class AlreadyExists(Exception):
+    pass
+
 @contextmanager
 def checkout_and_publish(branch: str, tag: str, release: str):
     os.makedirs(f"branch/{branch}", exist_ok=True)
@@ -15,7 +18,7 @@ def checkout_and_publish(branch: str, tag: str, release: str):
     os.chdir("renders")
     if os.system(f"git rev-parse --verify --quiet {tag}") == 0 and not devmode:
         print(f"Tag {tag} already exists, skipping")
-        return
+        raise AlreadyExists(f"Tag {tag} already exists, skipping")
     yield
     if not devmode:
         os.system("git add .")
@@ -40,40 +43,46 @@ def main(release: str):
         os.system("rm -rf branch")
     os.makedirs("branch", exist_ok=True)
 
-    with checkout_and_publish("renders", f"{release}-renders", release):
-        cwd = Path(os.getcwd())
-        load_dir = cwd / "resourcepack"
-        print(f"Running beet in {cwd}")
-        os.system("rm -rf resourcepack")
-        os.makedirs("resourcepack", exist_ok=True)
-        config = ProjectConfig(
-            pipeline=[
-                "model_resolver.plugins.render_all_vanilla",
-                "model_resolver.plugins.render_all_items",
-            ],
-            output=cwd,
-            meta={"model_resolver": {"minecraft_version": release}},
-            resource_pack={"load": load_dir, "name": load_dir.name},
-        )
-        with run_beet(config=config) as ctx:
-            pass
+    try:
+        with checkout_and_publish("renders", f"{release}-renders", release):
+            cwd = Path(os.getcwd())
+            load_dir = cwd / "resourcepack"
+            print(f"Running beet in {cwd}")
+            os.system("rm -rf resourcepack")
+            os.makedirs("resourcepack", exist_ok=True)
+            config = ProjectConfig(
+                pipeline=[
+                    "model_resolver.plugins.render_all_vanilla",
+                    "model_resolver.plugins.render_all_items",
+                ],
+                output=cwd,
+                meta={"model_resolver": {"minecraft_version": release}},
+                resource_pack={"load": load_dir, "name": load_dir.name},
+            )
+            with run_beet(config=config) as ctx:
+                pass
+    except AlreadyExists:
+        pass
 
-    with checkout_and_publish("renders-special", f"{release}-renders-special", release):
-        cwd = Path(os.getcwd())
-        load_dir = cwd / "resourcepack"
-        print(f"Running beet in {cwd}")
-        os.system("rm -rf resourcepack")
-        os.makedirs("resourcepack", exist_ok=True)
-        config = ProjectConfig(
-            pipeline=[
-                "model_resolver.plugins.render_all_items",
-            ],
-            output=cwd,
-            meta={"model_resolver": {"minecraft_version": release, "special_rendering": True}},
-            resource_pack={"load": load_dir, "name": load_dir.name},
-        )
-        with run_beet(config=config) as ctx:
-            pass
+    try:
+        with checkout_and_publish("renders-special", f"{release}-renders-special", release):
+            cwd = Path(os.getcwd())
+            load_dir = cwd / "resourcepack"
+            print(f"Running beet in {cwd}")
+            os.system("rm -rf resourcepack")
+            os.makedirs("resourcepack", exist_ok=True)
+            config = ProjectConfig(
+                pipeline=[
+                    "model_resolver.plugins.render_all_items",
+                ],
+                output=cwd,
+                meta={"model_resolver": {"minecraft_version": release, "special_rendering": True}},
+                resource_pack={"load": load_dir, "name": load_dir.name},
+            )
+            with run_beet(config=config) as ctx:
+                pass
+    except AlreadyExists:
+        pass
 
 
 
