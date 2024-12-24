b612e18cc34aca46bc3eb7ef947f95c199dd2dd5
edayot <pro.e.dayot@gmail.com>
2024-12-24 19:12:12 +0100

    feat: new special branch

--- main.py
+++ main.py
@@ -2,60 +2,78 @@ import os
 import shlex
 from pathlib import Path
 from beet import ProjectConfig, run_beet
+from contextlib import contextmanager
 
+devmode = False
 
-def main(release: str, devmode: bool = False):
-    print(f"Release {release}!")
-    release = shlex.quote(release)
-    if os.path.exists("branch"):
-        os.system("rm -rf branch")
-    os.makedirs("branch", exist_ok=True)
-    os.chdir("branch")
-    cloned_branch = "renders"
+@contextmanager
+def checkout_and_publish(branch: str, tag: str, release: str):
+    os.makedirs(f"branch/{branch}", exist_ok=True)
+    os.chdir(f"branch/{branch}")
     git_url = "https://github.com/edayot/renders.git"
-    os.system(f"git clone -b {cloned_branch} {git_url}")
+    os.system(f"git clone -b {branch} {git_url}")
     os.chdir("renders")
-    # check if the release tag already exists
-    if (
-        os.system(f"git rev-parse --verify --quiet {release}-renders") == 0
-        and not devmode
-    ):
-        print(f"Release {release} already exists!")
-        return
-
-    cwd = Path(os.getcwd())
-    load_dir = cwd / "resourcepack"
-    print(f"Running beet in {cwd}")
-    os.system("rm -rf resourcepack")
-    os.makedirs("resourcepack", exist_ok=True)
-    config = ProjectConfig(
-        pipeline=[
-            "model_resolver.plugins.render_all_vanilla",
-            "model_resolver.plugins.render_all_items",
-        ],
-        output=cwd,
-        meta={"model_resolver": {"minecraft_version": release}},
-        resource_pack={"load": load_dir, "name": load_dir.name},
-    )
-    with run_beet(config=config) as ctx:
-        pass
-
+    if os.system(f"git rev-parse --verify --quiet {tag}") == 0 and not devmode:
+        raise ValueError(f"Tag {tag} already exists!")
+    yield
     if not devmode:
         os.system("git add .")
         os.system(f"git commit -m '✨ Generate renders for {release}' --allow-empty")
-        os.system(
-            f"git tag -a '{release}-renders' -m '✨ Generate renders for {release}'"
-        )
-
+        os.system(f"git tag -a '{tag}' -m '✨ Generate renders for {release}'")
         if "GITHUB_TOKEN" in os.environ:
             token = os.environ["GITHUB_TOKEN"]
             os.system(
                 f"git remote set-url origin https://github-actions:{token}@{git_url.replace('https://', '')}"
             )
         os.system("git push origin renders --tags")
-        os.chdir("../..")
-        os.system("rm poetry.lock")
+    os.chdir("../../..")
+    if not devmode:
+        os.system(f"rm -rf branch/{branch}")
+    
+
+
+def main(release: str):
+    print(f"Release {release}!")
+    release = shlex.quote(release)
+    if os.path.exists("branch"):
         os.system("rm -rf branch")
+    os.makedirs("branch", exist_ok=True)
+
+    with checkout_and_publish("renders", f"{release}-renders", release):
+        cwd = Path(os.getcwd())
+        load_dir = cwd / "resourcepack"
+        print(f"Running beet in {cwd}")
+        os.system("rm -rf resourcepack")
+        os.makedirs("resourcepack", exist_ok=True)
+        config = ProjectConfig(
+            pipeline=[
+                "model_resolver.plugins.render_all_vanilla",
+                "model_resolver.plugins.render_all_items",
+            ],
+            output=cwd,
+            meta={"model_resolver": {"minecraft_version": release}},
+            resource_pack={"load": load_dir, "name": load_dir.name},
+        )
+        with run_beet(config=config) as ctx:
+            pass
+
+    with checkout_and_publish("renders-special", f"{release}-renders-special", release):
+        cwd = Path(os.getcwd())
+        load_dir = cwd / "resourcepack"
+        print(f"Running beet in {cwd}")
+        os.system("rm -rf resourcepack")
+        os.makedirs("resourcepack", exist_ok=True)
+        config = ProjectConfig(
+            pipeline=[
+                "model_resolver.plugins.render_all_items",
+            ],
+            output=cwd,
+            meta={"model_resolver": {"minecraft_version": release, "special_rendering": True}},
+            resource_pack={"load": load_dir, "name": load_dir.name},
+        )
+        with run_beet(config=config) as ctx:
+            pass
+
 
 
 if __name__ == "__main__":
